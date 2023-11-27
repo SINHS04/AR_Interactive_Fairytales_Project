0e29b1f77cc16eee9bee4228ecd10685a373ca7e
CHANIVER <cksdlqj@gmail.com>
2023-11-27 17:40:59 +0900

    추가: effect / 간단한 scene전환(3)

---
+++ AR_Interactive_Fairytales/Library/Bee/Android/Prj/IL2CPP/Il2CppBackup/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,59 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void ARSceneManager_GotoMain_m883DA3C30AFEDBBD5566A550F224BF2248949157 (void);
+extern void ARSceneManager_GotoReading_mA784F2518735B688F1EF0B306BDBB2FDA9B5C3C7 (void);
+extern void ARSceneManager_GotoScene_mBAE9C27FB450773559385511F9A54CACDD87C65C (void);
+extern void ARSceneManager__ctor_mE4E6F27768FB29E0CB70AB2B1859CF64F64CF0F1 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+static Il2CppMethodPointer s_methodPointers[6] = 
+{
+	ARSceneManager_GotoMain_m883DA3C30AFEDBBD5566A550F224BF2248949157,
+	ARSceneManager_GotoReading_mA784F2518735B688F1EF0B306BDBB2FDA9B5C3C7,
+	ARSceneManager_GotoScene_mBAE9C27FB450773559385511F9A54CACDD87C65C,
+	ARSceneManager__ctor_mE4E6F27768FB29E0CB70AB2B1859CF64F64CF0F1,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+};
+static const int32_t s_InvokerIndices[6] = 
+{
+	6059,
+	6059,
+	4744,
+	6059,
+	9264,
+	6059,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	6,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
